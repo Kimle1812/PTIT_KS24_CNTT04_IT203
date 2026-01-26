9aa9597e7e51061f9a5faa9531e143b95dc7c5ef
Kimle1812 <nguyenthikimle18122003@gmail.com>
2026-01-26 08:28:51 +0700

    Add files via upload

---
+++ PTIT_CNTT4_IT203_Session01/Exercise02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so ngay cham tre: ");
+        int n = sc.nextInt();
+
+        System.out.print("Nhap so luong sach muon: ");
+        int m = sc.nextInt();
+
+        double total = n * m * 5000;
+        System.out.println("Tien phat goc: " + total + " VND");
+
+        if (n > 7 && m >= 3) {
+            total = total * 1.2;
+        }
+
+        System.out.println("Tien phat sau dieu chinh: " + total + " VND");
+
+        boolean lockCard = total > 50000;
+        System.out.println("Yeu cau khoa the: " + lockCard);
+    }
+}
