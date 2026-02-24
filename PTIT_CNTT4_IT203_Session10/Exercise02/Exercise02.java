d942738e9d456925c667db0528a67218674bbe58
Kimle1812 <nguyenthikimle18122003@gmail.com>
2026-02-24 22:50:41 +0700

    Add files via upload

---
+++ PTIT_CNTT4_IT203_Session10/Exercise02/Exercise02.java
@@ -0,0 +1,47 @@
+package PTIT_CNTT4_IT203_Session10.Exercise02;
+
+public class Exercise02 {
+    static abstract class Vehicle {
+
+        protected String brand;
+
+        public Vehicle(String brand) {
+            this.brand = brand;
+        }
+
+        public abstract void move();
+    }
+
+    static class Car extends Vehicle {
+
+        public Car(String brand) {
+            super(brand);
+        }
+
+        @Override
+        public void move() {
+            System.out.println(brand + " - Cách di chuyển: Di chuyển bằng động cơ");
+        }
+    }
+
+    static class Bicycle extends Vehicle {
+
+        public Bicycle(String brand) {
+            super(brand);
+        }
+
+        @Override
+        public void move() {
+            System.out.println(brand + " - Cách di chuyển: Di chuyển bằng sức người");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle car = new Car("Toyota");
+        Vehicle bicycle = new Bicycle("Giant");
+
+        car.move();
+        bicycle.move();
+    }
+}
---
+++ PTIT_CNTT4_IT203_Session10/Exercise04/Exercise04.java
@@ -0,0 +1,25 @@
+package PTIT_CNTT4_IT203_Session10.Exercise04;
+
+public class Exercise04 {
+    interface RemoteControl {
+
+        void powerOn();
+
+        default void checkBattery() {
+            System.out.println("Pin ổn định");
+        }
+    }
+
+    public static void main(String[] args) {
+        RemoteControl smartLight = new RemoteControl() {
+
+            @Override
+            public void powerOn() {
+                System.out.println("Đèn đã bật");
+            }
+        };
+
+        smartLight.powerOn();
+        smartLight.checkBattery();
+    }
+}
