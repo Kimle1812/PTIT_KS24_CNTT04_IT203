a69c000869259ec80e205e22731720c94d437c64
Kimle1812 <nguyenthikimle18122003@gmail.com>
2026-03-01 23:40:30 +0700

    Add files via upload

---
+++ PTIT_CNTT4_IT203_Session14/Exercise01/Exercise01.java
@@ -0,0 +1,26 @@
+package PTIT_CNTT4_IT203_Session14.Exercise01;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        Set<String> danhSachCho = new LinkedHashSet<>();
+
+        String[] input = {
+                "Nguyễn Văn A – Yên Bái",
+                "Trần Thị B – Thái Bình",
+                "Nguyễn Văn A – Yên Bái",
+                "Lê Văn C – Hưng Yên"
+        };
+
+        for (String benhNhan : input) {
+            danhSachCho.add(benhNhan);
+        }
+
+        System.out.println("Danh sách gọi khám:");
+        for (String ten : danhSachCho) {
+            System.out.println("- " + ten);
+        }
+    }
+}
